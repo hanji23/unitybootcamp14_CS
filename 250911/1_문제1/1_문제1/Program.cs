ec9f69abace0f6ce8b547400e9fecd823e967ae4
jihan <hanji797979@gmail.com>
2025-09-11 09:41:26 +0900

    250911

---
+++ 250911/1_문제1/1_문제1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _1_문제1
+{
+    internal class Program
+    {
+
+        static void Swap(ref int x, ref int y) 
+        {
+            int a = x;
+            x = y; 
+            y = a;   
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int x = 10;
+            int y = 20;
+
+            Swap(ref x, ref y);
+
+            Console.WriteLine($"{x} {y}");
+        }
+    }
+}
