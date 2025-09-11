4c1cd119d9c4b2a9e48d353d4643363bd77a33c9
jihan <hanji797979@gmail.com>
2025-09-11 15:21:06 +0900

    250911

---
+++ 250911/3_문제3/3_문제3/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _3_문제3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr =
+            {
+                {1 , 2, 3 },
+                { 4 , 5 , 6 },
+                { 7 , 8 , 9 }
+            };
+
+            //for (int i = 0; i < arr.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < arr.GetLength(0); j++)
+            //    {
+            //        Console.Write($"{ arr[i, j]}");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(0); j++)
+                {
+                    Console.Write($"{arr[j, i]}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
