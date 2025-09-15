f000b3eca9cbdc1b05da0f0d39114bf7a71bf689
jihan <hanji797979@gmail.com>
2025-09-15 16:44:08 +0900

    250915

---
+++ 250915/2_열거자/2_열거자/Program.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+
+namespace _2_열거자
+{
+    internal class Program
+    {
+        // 열거자(Enumerator) 
+        // 어떤 데이터 집합을 순서대로 하나씩 탐색하는 기능
+        // IEnumerable 인터페이스
+        // IEnumerator 인터페이스
+        // foreach -> 위의 두가지 인터페이스 개념위에 구축되어있음 (위에 두 인터페이스를 상속받은 상태로 돌아감)
+
+        /*
+                      [컬렉션] ──▶ GetEnumerator()
+                         │
+                         ▼
+                ┌───────────────────┐
+                │  Enumerator 객체   │
+                │───────────────────│
+                │ + MoveNext()      │
+                │ + Current         │
+                └───────────────────┘
+                         │
+                         ▼
+                foreach 루프 실행
+
+         Start
+           │
+           ▼
+        [컬렉션]
+           │  GetEnumerator()
+           ▼
+        Enumerator 생성
+           │
+           ▼
+        while (MoveNext())
+           │
+           ├─ true ─▶ Current 읽기 ─▶ 루프 본문 실행 ─┐
+           │                                          │
+           └─ false ──────────────────────────────────┘
+           │
+           ▼
+           End
+        */
+
+        // 배열은 연속된 저장공간에 할당
+        //   []      []      []      []      [] 
+        // 0x1000  0x1004  0x1008  0x1012  0x1016
+
+        // indexer this[int index]
+
+        // arr[7]
+        // element_address = base_address + (index* sizeof(T))
+        // 사용자에게는 노출 하지 않고 컴파일/JIT 이 알아서 처리를 해줍니다. 
+
+        // Head ──▶ [10 | next] ──▶ [20 | next] ──▶ [30 | null]
+
+        /*
+         Start → node = head
+            │
+            ▼
+            MoveNext()
+            │ true
+            ▼
+            Current = node.Value (10)
+            node = node.Next
+            │
+            ▼
+            MoveNext()
+            │ true
+            ▼
+            Current = node.Value (20)
+            node = node.Next
+            │
+            ▼
+            MoveNext()
+            │ true
+            ▼
+            Current = node.Value (30)
+            node = node.Next (null)
+            │
+            ▼
+            MoveNext()
+            │ false
+            ▼
+            End
+         */
+
+        // 유니티 
+        // IEnumorator Test()
+        // {
+        //    yield => 내부에서 IEnumerator와 IEnumerable 인터페이스 자동구현 코드 생성
+        //    
+        //    return new waitForSecond(3);
+        //    Consoloe.WriteLine("sdsdsd");
+        // }
+
+
+        // IEnumorator MoveRight()
+        // {
+        //    // 오른쪽 으로만 이동하는 코드
+        // }
+
+        // void MoveUp()
+        // {
+        //    // 위쪽 으로만 이동하는 코드
+        // }
+
+        // [프레임루프]
+        //    |
+        //    ㄴ StartCourutine(MoveRight) -> 호출 스택과, 내부 IEnumerator 객체의 현재 실행 지점 기억함
+        //    ㄴ StartCourutine(MoveUp) -> 호출 스택과, 내부 IEnumerator 객체의 현재 실행 지점 기억함
+        //    ㄴ 다른 코루틴 있는지 확인?
+
+        // 코루틴 => 하나의 쓰레드에서 처리되는거다!
+        public class MyCollection : IEnumerable
+        {
+            private int[] _data;
+
+            public MyCollection(int[] data)
+            {
+                _data = data;
+            }
+
+            public IEnumerator GetEnumerator()
+            {
+                return new MyEnermerator(_data);
+            }
+
+            private class MyEnermerator : IEnumerator
+            {
+                private int[] _data;
+                private int _position = -1; // 시작전 위치
+
+                public MyEnermerator(int[] data)
+                {
+                    _data = data;
+                }
+
+                public object Current
+                {
+                    get
+                    {
+                        if (_position < 0 || _position >= _data.Length)
+                            throw new InvalidOperationException();
+                        return _data[_position];
+                    }
+                }
+
+                public bool MoveNext()
+                {
+                    _position++;
+                    return _position < _data.Length;
+                }
+
+                public void Reset()
+                {
+                    _position = -1;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var myData = new MyCollection(new int[] { 10, 20, 30 });
+
+            foreach (int item in myData)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ 250915/3_dfs_bfs/3_dfs_bfs/Program.cs
@@ -0,0 +1,152 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace _3_dfs_bfs
+{
+    class Graph
+    {
+        int[,] adj = new int[6, 6] //인접행렬방식
+        {
+            { 0, 1, 0, 1, 0, 0, },
+            { 1, 0, 1, 1, 0, 0, },
+            { 0, 1, 0, 0, 0, 0, },
+            { 1, 1, 0, 0, 0, 0, },
+            { 0, 0, 0, 0, 0, 1, },
+            { 0, 0, 0, 0, 1, 0, }
+
+            //{ 0, 1, 0, 1, 0, 0, },
+            //{ 1, 0, 1, 1, 0, 0, },
+            //{ 0, 1, 0, 0, 0, 0, },
+            //{ 1, 1, 0, 0, 1, 0, },
+            //{ 0, 0, 0, 1, 0, 1, },
+            //{ 0, 0, 0, 0, 1, 0, }
+        };
+
+        bool[] visited = new bool[6];
+
+        public void DFS(int now)
+        {
+            //1. now 부터 방문 후 방문 체크
+            Console.WriteLine($"방문 : {now}");
+            visited[now] = true;
+
+            //2. now와 연결된 정점들을 하나씩 확인해서 아직 방문하지 않은 정점을 방문
+            for (int next= 0;next < adj.GetLength(0); next++)
+            {
+                //배열을 초과하는지 확인
+
+                //연결되어있는지 확인
+                if (adj[now, next] == 0)
+                    continue;
+
+                //이미 방문 했는지 확인
+                if (visited[next] == true)
+                    continue;
+
+                DFS(next);
+            }
+        }
+
+        public void SearchAll()
+        {
+            visited = new bool[6];
+            for(int now = 0; now < adj.GetLength(0); now++)
+            {
+                if (visited[now] == false)
+                    DFS(now);
+            }
+        }
+    }
+
+
+    internal class Program
+    {
+        static int[,] adj = new int[50, 50];
+        static bool[,] visited = new bool[50, 50];
+        static int[] dy = { -1, 0, 1, 0 };
+        static int[] dx = { 0, -1, 0, 1 };
+
+        static int m, n, k;
+        static int ret = 0;
+
+        static void DFS(int y, int x)
+        {
+            // 방문 처리
+            visited[y, x] = true;
+
+            //연결되어있는 노드 확인해서 아직 방문안한녀석 방문
+            for (int i = 0; i < dy.Length; i++)
+            {
+                int newY = y + dy[i];
+                int newX = x + dx[i];
+
+                if (newY < 0 || newX < 0 || newY >= n || newX >= m)
+                    continue;
+
+                //배추가 있는지 확인
+                if (adj[newY, newX] == 0)
+                    continue;
+
+                //이미 방문 했는지 확인
+                if (visited[newY, newX] == true)
+                    continue;
+
+                DFS(newY, newX);
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Graph g = new Graph();
+            g.DFS(2);
+
+            g.SearchAll();
+
+            int t = int.Parse(Console.ReadLine());
+
+            while (t-- > 0) 
+            {
+                ret = 0;
+                string[] s = Console.ReadLine().Split();
+
+                m = int.Parse(s[0]); 
+                n = int.Parse(s[1]);
+                k = int.Parse(s[2]);
+
+                Array.Clear(adj);
+                Array.Clear(visited);
+                //adj = new int[50, 50];
+
+                int x, y;
+                for (int i = 0; i < k; i++)
+                {
+                    s = Console.ReadLine().Split();
+                    x = int.Parse(s[0]);
+                    y = int.Parse(s[1]);
+                    adj[y, x] = 1;
+                }
+
+                for (int now = 0; now < n; now++)
+                {
+                    for (int next = 0; next < m; next++)
+                    {            
+                        //배추가 있는지 확인
+                        if (adj[now, next] == 0)
+                            continue;
+
+                        //이미 방문 했는지 확인
+                        if (visited[now, next] == true)
+                            continue;
+
+                        DFS(now, next);
+                        ret++;
+                    }
+                    
+                }
+
+                Console.WriteLine(ret);
+                //t--;
+            }
+        }
+    }
+}
